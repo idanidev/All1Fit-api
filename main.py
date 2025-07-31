a938a608209dd0eec04daba5711669e293059294
idanidev <idanideveloper@gmail.com>
2025-07-31 23:12:41 +0200

    Delete y rutas.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
-from entrenamientos import router as entrenamientos_router
+from fastapi.middleware.cors import CORSMiddleware
+from entrenamientos import router
 
 app = FastAPI()
 
-app.include_router(entrenamientos_router)
+# Configuración CORS para permitir acceso desde tu frontend Angular
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Incluir rutas desde el router
+app.include_router(router)
